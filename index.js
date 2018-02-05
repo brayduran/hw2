3b86536cabd0304bf5ea12da9b7c266ddf65bad5
Brayden Duran <brayden.duran@kellogg.northwestern.edu>
2018-02-05 11:28:49 -0600

    thumbnail click

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here:
+$('#thumbnails img').click(function(){
+    $('#main-image').attr('src',$(this).attr('src').replace('thumbnails','main-image'));
+    $('#description').html($(this).attr('alt'));
+})
