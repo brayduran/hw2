e0a7e22bf90f1f6a5cf290f694ede846871ef5e4
Brayden Duran <brayden.duran@kellogg.northwestern.edu>
2018-02-07 11:49:09 -0600

    simplified code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Your code goes here:
 $('#thumbnails img').click(function(){
     $('#main-image').attr('src',$(this).attr('src').replace('thumbnails','main-image'));
-    $('#description').html($(this).attr('alt'));
 })
